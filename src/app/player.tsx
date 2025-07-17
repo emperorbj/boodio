1df92553bd6d7a11d58be6e6bcf5752db5b2b2ed
emperorbj <cwaku96@gmail.com>
2025-07-17 18:49:23 +0100

    added a player screen to with its router

---
+++ src/app/player.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const player = () => {
+  return (
+    <View className='flex-1 items-center justify-center'>
+      <Text className='text-2xl text-red-500'>player</Text>
+    </View>
+  )
+}
+
+export default player
--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,7 +1,7 @@
-import {Text, View,Image } from 'react-native';
+import {Text, View,Image,Pressable } from 'react-native';
 import {AntDesign} from '@expo/vector-icons';
 import {Book} from '../../types/data';
-
+import { Link } from 'expo-router';
 
 type BookListProps = {
     book: Book;
@@ -9,16 +9,18 @@ type BookListProps = {
 
 const BookList = ({book}: BookListProps ) => {
   return (
-      <View className="flex-row justify-center gap-4">
+
+    <Link href="/player" asChild>
+      <Pressable className="flex-row justify-center gap-4">
         <Image source={{uri:book.thumbnail_url}} className="w-20 aspect-square rounded-lg"/>
 
         <View className="flex-1 gap-1">
           <Text className="text-lg text-orange-500">{book.title}</Text>
           <Text className="text-sm text-white">{book.author}</Text>
         </View>
-        
-        <AntDesign name="playcircleo" size={24} color="gainsboro" className="text-center mt-4"/>
-      </View>
+        <AntDesign name="playcircleo" size={24} color="gainsboro" className="text-center mt-4 mr-6"/>
+      </Pressable>
+      </Link>
   )
 }
 
