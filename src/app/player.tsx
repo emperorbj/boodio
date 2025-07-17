bb3e461ba6384d2715242ddc9155662e7da227ac
emperorbj <cwaku96@gmail.com>
2025-07-17 19:01:42 +0100

    added a back route on the player.tsx component

--- src/app/player.tsx
+++ src/app/player.tsx
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text,Pressable } from 'react-native'
 import React from 'react'
+import Entypo from '@expo/vector-icons/Entypo';
+import { router } from 'expo-router';
+
+
+
 
 const player = () => {
+    
   return (
-    <View className='flex-1 items-center justify-center'>
-      <Text className='text-2xl text-red-500'>player</Text>
+    <View className='flex-1 bg-slate-900 items-center justify-center'>
+        <Pressable onPress={()=> router.back()} className='absolute top-16 left-5 p-2 rounded-full bg-slate-800'>
+        <Entypo name="chevron-down" size={24} color="white" />
+        </Pressable>
     </View>
   )
 }
