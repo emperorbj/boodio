40d08dbf74b42a941d493e5cdef0c9336a683618
emperorbj <cwaku96@gmail.com>
2025-07-22 19:02:44 +0100

    added the favorite screen and the functionality

--- src/app/(tabs)/likes.tsx
+++ src/app/(tabs)/likes.tsx
@@ -1,10 +1,51 @@
-import { View, Text } from 'react-native'
+import { View, Text,ActivityIndicator,FlatList, } from 'react-native'
 import React from 'react'
+import { useQuery } from '@tanstack/react-query'
+import { supabase } from '../../../lib/supabase'
+import FavoriteBooksList from '../../components/FavoriteList'
+import { StatusBar } from 'expo-status-bar'
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
-const likes = () => {
+
+const   likes = () => {
+
+  const fetchLikes = async () => {
+    const {data,error} = await supabase.from('library').select('*,books(*)')
+    if(error){
+      throw error
+    }
+    
+    return data
+  }
+
+  const {data,isLoading,error} = useQuery({
+    queryKey:['likes'],
+    queryFn:fetchLikes
+  })
+
+  if(isLoading) return (
+    <View className="flex-1 justify-center bg-slate-900 pt-20 p-4">
+      <ActivityIndicator size={"large"} color={'#ffffff'}/>
+    </View>
+  )
+
+  if(error) return  (
+    <View className="flex-1 items-center bg-slate-900 justify-center">
+      <Text className="text-red-500 text-2xl">{error.message}</Text>
+    </View>
+  )
   return (
-    <View>
-      <Text>likes</Text>
+    <View className='flex-1 justify-center  bg-slate-900 pt-20 p-4'>
+      <View className='flex-row items-center justify-between mb-4'>
+        <Text className='text-2xl text-white font-bold mb-4'>Favorites{" "}😍</Text>
+      </View>
+      <FlatList
+      data={data}
+      keyExtractor={(item) => item.books.id}
+      renderItem={({item})=>(<FavoriteBooksList book={item.books} />)}
+      contentContainerClassName='gap-4'
+      /> 
+       <StatusBar style="auto" />
     </View>
   )
 }
--- src/app/(tabs)/playlist.tsx
+++ src/app/(tabs)/playlist.tsx
@@ -1,12 +1,57 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import "../../../global.css"
+import { StatusBar } from 'expo-status-bar';
+import {FlatList, View,Image,ActivityIndicator,Text } from 'react-native';
+import books from '../../dummyBooks';
+import { useQuery } from "@tanstack/react-query";
+import { supabase } from "../../../lib/supabase";
+import SelectBook from "../../components/SelectBook";
 
-const playlist = () => {
-  return (
-    <View>
-      <Text>playlist</Text>
-    </View>
+
+export default function  PlayList() {
+
+  const fetchBooks = async () => {
+      const {data,error} = await supabase.from('books').select('*')
+      if(error){
+        throw error
+      }
+      return data
+    
+  }
+
+  const {data,isLoading,error} = useQuery({
+    queryKey:['books'],
+    queryFn:fetchBooks
+  })
+
+  if(isLoading) return (
+  <View className="flex-1 justify-center bg-slate-900 pt-20 p-4">
+    <ActivityIndicator size={"large"} color={'#ffffff'}/>
+  </View>
   )
+
+  if(error) return  (
+  <View className="flex-1 items-center bg-slate-900 justify-center">
+    <Text className="text-red-500 text-2xl">{error.message}</Text>
+  </View>
+  )
+
+
+
+
+  return (
+    <View className="flex-1 justify-center bg-slate-900 pt-20 p-4">
+       <View className='flex-row items-center justify-between mb-4'>
+          <Text className='text-2xl text-white font-bold mb-4'>Add to Library{" "}✒️</Text>
+        </View>
+      <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      renderItem={({item}) => <SelectBook book={item} />}
+      contentContainerClassName="gap-4"
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
-export default playlist
+
